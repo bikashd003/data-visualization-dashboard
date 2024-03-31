694daaf485f14866d32eebff19222e6da7c225c2
Bikash Das <bikashd003@gmail.com>
2024-03-31 12:32:34 +0530

    modified slide bar, make it slide drawer

--- Client/src/Context/ContextStore.tsx
+++ Client/src/Context/ContextStore.tsx
@@ -2,17 +2,19 @@ import React, { createContext, useState } from "react";
 
 interface DataContextType {
   open: boolean;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setOpen: (value: boolean) => void;
 }
 
 const DataContext = createContext<DataContextType | undefined>(undefined);
 
-const DataProvider = ({ children }:any) => {
+const DataProvider:React.FC = ({ children }: any) => {
   const [open, setOpen] = useState(false);
 
-
-
-  return <DataContext.Provider value={{open,setOpen}}>{children}</DataContext.Provider>;
+  return (
+    <DataContext.Provider value={{ open, setOpen }}>
+      {children}
+    </DataContext.Provider>
+  );
 };
 
 export { DataContext, DataProvider };
--- Client/src/Pages/Dashboard.tsx
+++ Client/src/Pages/Dashboard.tsx
@@ -1,21 +1,67 @@
-import { lazy,Suspense } from 'react';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-const LineChart = lazy(() => import('../Components/LineChart'));
+import { lazy, Suspense, useContext } from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+const LineChart = lazy(() => import("../Components/LineChart"));
 
-const BarChartComponent = lazy(() => import('../Components/BarChartComponent'));
-const PieChart = lazy(() => import('../Components/PieChart')); 
+const BarChartComponent = lazy(() => import("../Components/BarChartComponent"));
+const PieChart = lazy(() => import("../Components/PieChart"));
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import Navbar from "../Components/Navbar";
+import { DataContext } from "../Context/ContextStore";
+import { Drawer, List, ListItem, ListItemText } from "@mui/material";
 
-import Box from '@mui/material/Box';
-import PersistentDrawerLeft from "../Components/PersistantDrawer"
+
+const drawerWidth = 200;
 const Dashboard = () => {
+  const { open, setOpen } = useContext(DataContext)!;
 
+  const handleMenuClick = () => {
+    setOpen(!open);
+  };
+  const handleItemClick = () => {
+    setOpen(false);
+  }
   return (
     <>
- <Box sx={{ display: 'flex' }}>
-    <PersistentDrawerLeft />
-      <Grid container spacing={2} padding={4} marginTop={5} >
+      <Navbar onMenuClick={handleMenuClick} />
+      <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+        },
+      }}
+      variant="temporary"
+      anchor="left"
+      open={open}
+      onClose={() => setOpen(false)}
+      ModalProps={{
+        keepMounted: true,
+      }}
+    >
+      <List sx={{padding:"1vw"}}>
+        {['Dashboard', 'Analytics', 'Settings'].map((text) => (
+          <ListItem
+            key={text}
+            sx={{
+              cursor: 'pointer',
+              borderRadius:"50px",
+              '&:hover': {
+                backgroundColor: (theme) => theme.palette.action.hover,
+              },
+            }}
+            onClick={handleItemClick}
+          >
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+      <Grid container spacing={2} paddingLeft={4} paddingRight={4} paddingTop={2}>
         <Grid item xs={12} md={6} lg={8}>
           <Paper elevation={3} sx={{ p: 2 }}>
             <Typography variant="h6" gutterBottom>
@@ -26,7 +72,7 @@ const Dashboard = () => {
             </Suspense>
           </Paper>
         </Grid>
- 
+
         <Grid item xs={12} md={6} lg={4}>
           <Paper elevation={3} sx={{ p: 2 }}>
             <Typography variant="h6" gutterBottom>
@@ -47,8 +93,14 @@ const Dashboard = () => {
             </Suspense>
           </Paper>
         </Grid>
+        <Grid item xs={12} md={6} lg={4}>
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar />
+          </LocalizationProvider>
+          </Paper>
+        </Grid>
       </Grid>
-      </Box>
     </>
   );
 };
