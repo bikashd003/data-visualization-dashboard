ae82e4513621a5ec0ce080ac5d3ed9bbf9c708c7
Bikash Das <bikashd003@gmail.com>
2024-04-01 14:32:43 +0530

    added the backend deployed API endpoint

--- Client/src/Components/EditModal.tsx
+++ Client/src/Components/EditModal.tsx
@@ -60,7 +60,7 @@ const EditModal: React.FC = () => {
   const handleUpdate = async () => {
     if (!editedRow) return;
     try {
-     const res= await axios.put(`${API}/update-product/${selectedRow?._id}`, editedRow,{ headers: { Authorization: `${sessionStorage.getItem("token")}` } });
+     await axios.put(`${API}/update-product/${selectedRow?._id}`, editedRow,{ headers: { Authorization: `${sessionStorage.getItem("token")}` } });
       setSnackbarOpen(true);
       setSuccess(true);
       handleClose();
--- Client/src/Helpers/Api.ts
+++ Client/src/Helpers/Api.ts
@@ -1,1 +1,2 @@
-export const API="http://localhost:3000"
+// export const API="http://localhost:3000"
+export const API="https://data-visualization-dashboard-server.onrender.com"
